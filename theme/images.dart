c2852d4aa4d1e0bc9aa6f8bfae75fa7909aefa65
SaulUrrea <saul5577@gmail.com>
2021-10-27 16:16:06 -0500

    feat: se elimina codigo inecesario

--- theme/images.dart
+++
@@ -1,7 +0,0 @@
-final String assetBackground = 'assets/backgraund.png';
-final String assetIconNotification = 'assets/notification.png';
-final String assetIconDog = 'assets/perro.png';
-final String assetBannerDog = 'assets/opcion1.png';
-final String assetIconFlowers = 'assets/planta.png';
-final String assetBannerFlowers = 'assets/opcion2.png';
-final String assetBannerAugmentedReality = 'assets/opcion3.png';
